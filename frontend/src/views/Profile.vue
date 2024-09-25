e2e1558be6d20b77e6645fd1ac0bd2c053774a92
Mailinh <122569872+MailinhTA@users.noreply.github.com>
2024-09-25 12:14:16 -0400

    Update Profile.vue
    Integrate the profile plugin to show the informations of the profile and allows to modify its own data

--- frontend/src/views/Profile.vue
+++ frontend/src/views/Profile.vue
@@ -1,11 +1,19 @@
-<script setup>
+<script>
+import Profile from "@/components/Profile.vue";
+export default {
+  name: "App",
+  components: { Profile },
 
+  methods: {
+    openLogin() {
+      this.isLoginModalVisible = true; // Ouvrir la modal
+    },
+  },
+};
 </script>
 
 <template>
-
+  <Profile></Profile>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
