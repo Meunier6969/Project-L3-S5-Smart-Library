d095fc4ad5daef27ba0903e0d38da24ef4edc8cd
Corentin_k_ <Corentin-k@users.noreply.github.com>
2024-09-30 16:45:23 -0400

    Add the import of Admin component

--- frontend/src/router/router.js
+++ frontend/src/router/router.js
@@ -2,7 +2,7 @@
 
 import { createRouter, createWebHistory } from 'vue-router';
 import Home from "@/views/Home.vue";
-
+import Admin from "@/views/Admin.vue";
 // Configuration des routes
 const routes = [
 
